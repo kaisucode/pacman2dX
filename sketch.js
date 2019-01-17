8cc4acb274ac0ef20c56355281e0c8c41b68ff84
Kaisu <kevin.hsukaihao@gmail.com>
2019-01-17 11:54:32 -0500

    Basic but ugly movement keys

--- sketch.js
+++ sketch.js
@@ -5,12 +5,26 @@ class Pacman {
     this.r = 50;
   }
   update() {
-    this.pos.add(this.pos);
+    this.pos.add(this.vel);
   }
   display() {
     fill(255,255,255);
     arc(this.pos.x, this.pos.y, this.r, this.r, PI/6, 11*PI/6);
   }
+
+	moveRight(){
+    this.vel = createVector(2,0);
+	}
+	moveLeft(){
+    this.vel = createVector(-2,0);
+	}
+	moveUp() {
+    this.vel = createVector(0,-2);
+	}
+	moveDown() {
+    this.vel = createVector(0,2);
+	}
+
 }
 
 let pacman;
@@ -20,5 +34,20 @@ function setup() {
 }
 function draw() {
   background(0, 0, 0);
+	keyPressed();
+	pacman.update();
   pacman.display();
 }
+
+function keyPressed() {
+	if (keyCode == 39)			// <Right Arrow>
+		pacman.moveRight();
+	if (keyCode == 37)	// <Left Arrow>
+		pacman.moveLeft();
+	if (keyCode == 38)	// <Up Arrow>
+		pacman.moveUp();
+	if (keyCode == 40)	// <Down Arrow>
+		pacman.moveDown();
+}
+
+
